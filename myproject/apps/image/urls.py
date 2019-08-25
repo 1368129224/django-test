b93ea04df1989e35da1fb57894f878d962f56cc6
zooter_z <zooter_z@126.com>
2019-08-25 12:49:59 +0800

    image

--- myproject/apps/image/urls.py
+++ myproject/apps/image/urls.py
@@ -5,4 +5,5 @@ app_name = 'image'
 urlpatterns = [
     path('list-images/', views.list_images, name='list_images'),
     path('upload-image/', views.upload_image, name='upload_image'),
+    path('del-image/', views.del_image, name='del_image'),
 ]
--- myproject/apps/image/views.py
+++ myproject/apps/image/views.py
@@ -10,7 +10,7 @@ from .models import Image
 @login_required(login_url='/account/login/')
 @require_POST
 def upload_image(request):
-    form = ImageForm(data=require_POST)
+    form = ImageForm(data=request.POST)
     if form.is_valid():
         try:
             new_item = form.save(commit=False)
@@ -24,4 +24,15 @@ def upload_image(request):
 def list_images(request):
     images = Image.objects.filter(user=request.user)
     return render(request, 'image/list_images.html', {'images': images})
+
+@login_required(login_url='/account/login/')
+@require_POST
+def del_image(request):
+    image_id = request.POST['image_id']
+    try:
+        image = Image.objects.get(id=image_id)
+        image.delete()
+        return JsonResponse({'status':'1'})
+    except:
+        return JsonResponse({'status':'0'})
 
